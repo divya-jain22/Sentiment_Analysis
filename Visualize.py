88c8619aeabc58ed337c01d30ecc8b2a3ed04656
divya-jain22 <139007191+divya-jain22@users.noreply.github.com>
2023-07-28 13:52:58 +0530

    Visualize.py

--- Visualize.py
+++ Visualize.py
@@ -1,6 +1,6 @@
 # To visualize the output the following code will work
 # Here we have used matplotib library to visualize the data
-# The code will genetate the output as given as in file visual.png
+# The code will genetate the output in the bar chart form.
 
 from matplotlib import pyplot at plt
 fig, ax1 = plt.subplots()
