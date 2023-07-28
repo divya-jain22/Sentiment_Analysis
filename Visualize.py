841a198713e59f84ed5016cc6c1e8b87bab9be15
divya-jain22 <139007191+divya-jain22@users.noreply.github.com>
2023-07-28 13:21:12 +0530

    Visualize.py

---
+++ Visualize.py
@@ -0,0 +1,9 @@
+# To visualize the output the following code will work
+# Here we have used matplotib library to visualize the data
+# The code will genetate the output as given as in file visual.png
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
